8397b70edb23509839e55986acc6502e01221603
Jacob Jahneke <jacobjahneke@gmail.com>
2018-06-25 16:02:57 -0500

    weather again

--- LemonadeStand/LemonadeStand/Weather.cs
+++ LemonadeStand/LemonadeStand/Weather.cs
@@ -8,11 +8,12 @@ namespace LemonadeStand
 {
     public class Weather
     {
-        public string finalWeather;
-        public string temperature;
-        public string condition;
-
-        public string description;
+        public string forecastWeather;
+        public int forecastTemperature;
+        public string forecastCondition;
+        public int actualTemperature;
+        public string actualCondition;
+        public int motherNature;
 
         public Weather()
         {
@@ -22,50 +23,106 @@ namespace LemonadeStand
         public void TemperatureGeneration()
         {
             Random rnd = new Random();
-            int numberSelectionOne = rnd.Next(55, 99);
-            temperature = numberSelectionOne.ToString();
+            forecastTemperature = rnd.Next(55, 99);
         }
 
 
-        public void WeatherConditionRoll()
+        public void ForecastWeatherConditionRoll()
         {
-            Random rnd = new Random();
-            int numberSelectionTwo = rnd.Next(1, 7);
-
+            int numberSelectionTwo;
+            if (forecastTemperature <= 80)
+            {
+                Random rnd = new Random();
+                numberSelectionTwo = rnd.Next(3);
+            }
+            else 
+            {
+                Random rnd = new Random();
+                numberSelectionTwo = rnd.Next(1, 7);
+            }
+           
 
                 switch (numberSelectionTwo)
                 {
                     case 1:
-                        condition = "overcast";
+                        forecastCondition = "overcast";
                         break;
                     case 2:
-                        condition = "cloudy";
+                        forecastCondition = "cloudy";
                         break;
                     case 3:
-                        condition = "rain";
+                        forecastCondition = "rain";
                         break;
                     case 4:
-                        condition = "hot";
+                        forecastCondition = "hot";
                         break;
                     case 5:
-                        condition = "hazy";
+                        forecastCondition = "hazy";
                         break;
                     case 6:
-                       condition = "muggy";
+                       forecastCondition = "muggy";
                         break;
-                default:
+                    default:
                         break;
 
                 }
 
         }
 
-        public void FinalWeather()
+        public void ActualWeatherConditionRoll()
         {
-            finalWeather = "" + temperature + " degrees\n\n" + condition + "";
+            int numberSelectionThree;
+            if (actualTemperature <= 80)
+            {
+                Random rnd = new Random();
+                numberSelectionThree = rnd.Next(3);
+            }
+            else
+            {
+                Random rnd = new Random();
+                numberSelectionThree = rnd.Next(1, 7);
+            }
+
+
+            switch (numberSelectionThree)
+            {
+                case 1:
+                    forecastCondition = "overcast";
+                    break;
+                case 2:
+                    forecastCondition = "cloudy";
+                    break;
+                case 3:
+                    forecastCondition = "rain";
+                    break;
+                case 4:
+                    forecastCondition = "hot";
+                    break;
+                case 5:
+                    forecastCondition = "hazy";
+                    break;
+                case 6:
+                    forecastCondition = "muggy";
+                    break;
+                default:
+                    break;
+
+            }
+
         }
 
+        public void ForecastWeather()
+        {
+            forecastWeather = "" + forecastTemperature + " degrees\n\n" + forecastCondition + "";
+        }
 
+        public void ActualWeather()
+        {
+            Random rnd = new Random();
+            motherNature = rnd.Next(-11, 11);
+            actualTemperature = forecastTemperature + motherNature;
+
+        }
     }
 }
 
